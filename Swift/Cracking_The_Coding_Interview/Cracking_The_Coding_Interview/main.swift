3a02c43b298f4d5ade0a9c91d5d893e55c7f49e0
Stephen Clark <stephenclark@Stephens-MBP.fios-router.home>
2017-03-29 15:10:19 -0400

    Initial Commit

---
+++ Swift/Cracking_The_Coding_Interview/Cracking_The_Coding_Interview/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Cracking_The_Coding_Interview
+//
+//  Created by Stephen Clark on 3/29/17.
+//  Copyright © 2017 Stephen Clark. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

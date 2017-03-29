c29a069646cd23ab302edc8d46cb206c1e646914
Stephen Clark <stephenclark@Stephens-MBP.fios-router.home>
2017-03-29 15:47:05 -0400

    Java Directory Added

---
+++ Java/Cracking_The_Coding_Interview/src/Chapter.java
@@ -0,0 +1,5 @@
+
+public interface Chapter
+{
+	public void run();
+}
---
+++ Java/Cracking_The_Coding_Interview/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		Chapter1 chap1 = new Chapter1();
+		chap1.run();
+	}
+
+}

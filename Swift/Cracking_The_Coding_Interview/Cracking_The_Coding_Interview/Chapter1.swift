ab7f022c488b45c7515c8db497337b226cd72137
Stephen Clark <stephenclark@Stephens-MBP.fios-router.home>
2017-04-04 14:07:00 -0400

    Implemented isUnique function

--- Swift/Cracking_The_Coding_Interview/Cracking_The_Coding_Interview/Chapter1.swift
+++ Swift/Cracking_The_Coding_Interview/Cracking_The_Coding_Interview/Chapter1.swift
@@ -12,11 +12,34 @@ class Chapter1: Chapter
 {
     override func run()
     {
-        print(isUnique(str1: "ABC"))
+        print(isUnique(str1: "Steve"))
+        print(isUnique(str1: "ABCD"))
     }
     
+    /*
+        1.1 - isUnique()
+        
+        Implement an algorithm to determine if a string has all unique characters.  What if you
+        cannot use additional data structures?
+    */
     func isUnique(str1: String) -> Bool
     {
+        // Create a character array of the input string to loop through
+        let charArray = str1.characters
+        
+        // Check each character against every other character in the string.  If there is more than
+        // one instance return false
+        for i in charArray
+        {
+            var charCount: Int = 0
+            for j in charArray
+            {
+                if (i == j) { charCount += 1 }
+            }
+            
+            if (charCount > 1) { return false }
+        }
+        
         return true;
     }
 }
